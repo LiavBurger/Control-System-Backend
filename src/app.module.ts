aaa85ab1e9accc391449ba6f99198b03544eadd5
Liav Burger <62385332+LiavBurger@users.noreply.github.com>
2025-05-06 14:52:21 +0300

    refactor: rename volume and project names to 'control-system'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { FilesModule } from './files/files.module';
 import { AuthModule } from './auth/auth.module';
 import databaseConfig from './database/config/database.config';
 import appConfig from './config/app.config';
-import fileConfig from './files/config/file.config';
-import path from 'path';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import { ConfigModule } from '@nestjs/config';
 import { HomeModule } from './home/home.module';
-import { AllConfigType } from './config/config.type';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './database/mongoose-config.service';
 import cognitoConfig from './auth/config/cognito.config';
@@ -21,34 +16,10 @@ const infrastructureDatabaseModule = MongooseModule.forRootAsync({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [databaseConfig, appConfig, fileConfig, cognitoConfig],
+      load: [databaseConfig, appConfig, cognitoConfig],
       envFilePath: ['.env'],
     }),
     infrastructureDatabaseModule,
-    I18nModule.forRootAsync({
-      useFactory: (configService: ConfigService<AllConfigType>) => ({
-        fallbackLanguage: configService.getOrThrow('app.fallbackLanguage', {
-          infer: true,
-        }),
-        loaderOptions: { path: path.join(__dirname, '/i18n/'), watch: true },
-      }),
-      resolvers: [
-        {
-          use: HeaderResolver,
-          useFactory: (configService: ConfigService<AllConfigType>) => {
-            return [
-              configService.get('app.headerLanguage', {
-                infer: true,
-              }),
-            ];
-          },
-          inject: [ConfigService],
-        },
-      ],
-      imports: [ConfigModule],
-      inject: [ConfigService],
-    }),
-    FilesModule,
     AuthModule,
     HomeModule,
   ],
--- src/config/config.type.ts
+++ src/config/config.type.ts
@@ -1,11 +1,9 @@
 import { AppConfig } from './app-config.type';
 import { DatabaseConfig } from '../database/config/database-config.type';
-import { FileConfig } from '../files/config/file-config.type';
 import { CognitoConfig } from '../auth/config/cognito-config.type';
 
 export type AllConfigType = {
   app: AppConfig;
   auth: CognitoConfig;
   database: DatabaseConfig;
-  file: FileConfig;
 };
